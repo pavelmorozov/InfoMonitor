5fdb844bb6647ad74eaf924b178ea5ed16f4b32d
Pavlo Morozov <pavelmorozov@mail.ru>
2014-07-01 16:53:30 +0300

    TODO:JavaScript for ajax

--- src/main/webapp/resources/script.js
+++ src/main/webapp/resources/script.js
@@ -1,30 +1,16 @@
 $(document).ready(function() {
-	$('#test').submit(function(event) {
-		console.log("1000");
-		//AJAX();
-		event.preventDefault();
+    $("#test").click(function (event_test) {
+        //$(this).slideUp();
+        //alert(" ALERT ! ");
+        console.log('$("#test").click(function ()');
+        AJAX();
+        event_test.preventDefault();
 	});
 });
 
-//function AJAX() {
-//	var monitor = $('#monitor').data(val());
-//	var url = "show?monitorName="+monitor; 
-//	
-//	console.log(url);
-//	
-//	a = $.ajax({
-//		url     : $("#fillUserAccount").attr("action"),
-//		dataType: 'json',
-//		data    : "account=" + account+ "&amount=" + amount,  
-//		success : function(response){
-//			//alert (response);
-//			$('.hideAfterFill').css("display","none");
-//			$('#fillResult').text(response.text);
-//			$("#balance-"+account).text(response.balance);
-//			setTimeout('$(\'#fillUserAccountPopUp\').css(\"display\",\"none\")',3000);
-//		},
-//		error   : function (response,status,e){
-//			alert('error: '+e);
-//		}
-//	});
-//};
+function AJAX() {
+	var monitor = $('#monitor').text();
+	var url = "show?monitorName="+monitor;
+	console.log("AJAX()");
+	console.log(url);
+}
